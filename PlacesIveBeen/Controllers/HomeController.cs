c9e917c1e19fdbaa2a96ef5673b394c2c1d5aa1b
Benjamin McFarland <benrmcfarland@gmail.com>
2020-12-16 09:56:37 -0800

    Add all basic starting code
    Co-authored-by: Kaila Sprague McRae <kaila.sprague@icloud.com>

---
+++ PlacesIveBeen/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using PlacesIveBeen.Models;
+
+namespace PlacesIveBeen.Controllers
+{
+  public class HomeController : Controller
+  {
+
+    [Route("/")]
+    public ActionResult Index()
+    {
+      
+    }
+
+  }
+}
---
+++ PlacesIveBeen/Models/Place.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+
+namespace PlacesIveBeen.Models
+{
+  public class Place
+  {
+    public string Description { get; set; }
+    private static List<Place> _instances = new List<Place> {};
+
+    public Place(string description)
+    {
+      Description = description;
+      _instances.Add(this);
+    }
+
+    public static List<Place> GetAll()
+    {
+      return _instances;
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+
+  }
+}
